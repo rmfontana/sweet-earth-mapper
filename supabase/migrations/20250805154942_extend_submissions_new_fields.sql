067b3b5eb8823848e1c5c295e9377fd597cda411
Rose Fontana <rosemariafontana@gmail.com>
2025-08-05 12:31:47 -0400

    Didn't save migration

--- supabase/migrations/20250805154942_extend_submissions_new_fields.sql
+++ supabase/migrations/20250805154942_extend_submissions_new_fields.sql
@@ -4,3 +4,14 @@ ALTER TABLE public.submissions
   ADD COLUMN contributor_name text NULL,
   ADD COLUMN harvest_time text NULL,
   ADD COLUMN notes text NULL;
+
+ALTER TABLE public.submissions
+  RENAME COLUMN timestamp TO assessment_date;
+
+ALTER TABLE public.submissions
+  ADD COLUMN purchase_date date NULL,
+  RENAME COLUMN label TO crop_variety,
+  ADD COLUMN harvest_time text NULL,
+  ADD COLUMN farm_location text NULL,
+  ADD COLUMN contributor_name text NULL,
+  ADD COLUMN outlier_notes text NULL;
